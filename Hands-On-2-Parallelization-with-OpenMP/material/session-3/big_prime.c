b3738b257b391da6cd59042ddf1881ebf2c334c1
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-08-28 22:19:18 -0300

    parallelization hands on 2 session 3 big_prime

--- Hands-On-2-Parallelization-with-OpenMP/material/session-3/big_prime.c
+++ Hands-On-2-Parallelization-with-OpenMP/material/session-3/big_prime.c
@@ -12,6 +12,7 @@ HowToExecute:   OMP_NUM_THREADS=${num_threads} ./big_prime
 #include <stdio.h>
 #include <math.h>
 #include <limits.h>
+#include <omp.h>
 
 typedef unsigned long long big_integer;
 #define BIGGEST_INTEGER ULLONG_MAX
@@ -19,16 +20,55 @@ typedef unsigned long long big_integer;
 int is_prime(big_integer n)
 {
   int result;
-  big_integer sq_root;
+  big_integer sq_root, i;
 
   result = (n % 2 != 0 || n == 2);
 
   if (result)
   {
     sq_root = sqrt(n);
+    
+    omp_set_num_threads(4);
+    #pragma omp parallel private(i)
+    {
+      int id = omp_get_thread_num();
 
-    for (big_integer i = 3; result && i <= sq_root; i += 2)
-      result = n % i != 0;
+      if(id == 0)
+      {
+        i = 3;
+        while(result && i <= (int)sq_root/4){
+          result = n % i != 0;
+          i += 2;
+        }
+      }
+      if(id == 1)
+      {
+        i = (big_integer)sq_root/4;
+        while(result && i <= 2*sq_root/4){
+          result = n % i != 0;
+          i += 2;
+        }
+      }
+      if(id == 2)
+      {
+        i = 2*(big_integer)sq_root/4;
+        while(result && i <= 3*sq_root/4){
+          result = n % i != 0;
+          i += 2;
+        }
+      }
+      if(id == 3)
+      {
+        i = 3*(big_integer)sq_root/4;
+        while(result && i <= sq_root){
+          result = n % i != 0;
+          i += 2;
+        }
+      }
+    }
+    
+    // for (big_integer i = 3; result && i <= sq_root; i += 2)
+    //   result = n % i != 0;
   }
 
   return result;
@@ -37,11 +77,13 @@ int is_prime(big_integer n)
 int main(int argc, char **argv)
 {
   big_integer n;
-
+  double t1, t2;
+  t1 = omp_get_wtime();
   for (n = BIGGEST_INTEGER; !is_prime(n); n -= 2)
   {
   }
-
+  t2 = omp_get_wtime();
+  printf("Time execution: %lf\n", t2-t1);
   printf("%llu\n", n);
 
   return 0;

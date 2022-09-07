3587f72c45403c75a9376f4b6ecaad12d7a00027
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-09-07 19:48:17 -0300

    parallel basic operation

--- Hands-On-3-Portable-Parallel-Programming-with-MPI/material/sequential-basic-operations-handson3.c
+++ Hands-On-3-Portable-Parallel-Programming-with-MPI/material/sequential-basic-operations-handson3.c
@@ -1,37 +1,72 @@
-/*
-@(#)File:           $sequential-basic-operations-handson3.c$
-@(#)Last changed:   $Date: 20220407 11:35:00 $
-@(#)Purpose:        Basic operations in a sequential algorithm in C
-@(#)Author:         muriloboratto 'at' fieb.org.br
-@(#)Usage:
-@(*) Hotocompile:   gcc sequential-basic-operations-handson3.c -o obj
-@(*) Hotoexecute:   ./obj
-*/
-
 #include <stdio.h>
+#include <mpi.h>
 #define SIZE 12
 
-int main (int argc, char **argv){
-
-  int i, sum = 0, subtraction = 0, mult = 1;
+int main(int argc, char **argv)
+{
+  int i, sum = 0, subtraction = 0, mult = 1, result, value;
   int array[SIZE];
+  char ops[] = {'+', '-', '*'};
+  char operationsRec;
+  int numberProcess, id, to, from, tag = 1000;
 
-  for(i = 0; i < SIZE; i++)
-    array[i] = i + 1;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+  MPI_Comm_size(MPI_COMM_WORLD, &numberProcess);
+  MPI_Status status;
+  printf("id: %d\n\n", id);
 
-  for(i = 0; i < SIZE; i++)
-    printf("array[%d] = %d\n", i, array[i]);
+  if (id == 0)
+  {
+    for (i = 0; i < SIZE; i++)
+    {
+      array[i] = i + 1;
+      printf("%d %d\t", i, array[i]);
+    }
+    printf("\n");
+    for (to = 1; to < numberProcess; to++)
+    {
+      MPI_Send(&array, SIZE, MPI_INT, to, tag, MPI_COMM_WORLD);
+      MPI_Send(&ops[to - 1], 1, MPI_CHAR, to, tag, MPI_COMM_WORLD);
+    }
 
-  for(i = 0; i < SIZE; i++) {
-    sum = sum + array[i];
-    subtraction = subtraction - array[i];
-    mult = mult * array[i];
+    for (to = 1; to < numberProcess; to++)
+    {
+      MPI_Recv(&result, 1, MPI_INT, to, tag, MPI_COMM_WORLD, &status);
+      MPI_Recv(&operationsRec, 1, MPI_CHAR, to, tag, MPI_COMM_WORLD, &status);
+      printf("(%c) = %d\n", operationsRec, result);
+    }
   }
+  else
+  {
+    MPI_Recv(&array, SIZE, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+    MPI_Recv(&operationsRec, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
 
-  printf("Sum = %d\n", sum);
-  printf("Subtraction = %d\n", subtraction);
-  printf("Multiply = %d\n", mult);
+    switch (operationsRec)
+    {
+    case '+':
+      value = 0;
+      for (i = 0; i < SIZE; i++)
+        value += array[i];
+      break;
+    case '-':
+      value = 0;
+      for (i = 0; i < SIZE; i++)
+        value -= array[i];
+      break;
+    case '*':
+      value = 1;
+      for (i = 0; i < SIZE; i++)
+        value *= array[i];
+      break;
+    }
+    MPI_Send(&value, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+    MPI_Send(&operationsRec, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD);
+  }
+  // printf("Sum = %d\n", sum);
+  // printf("Subtraction = %d\n", subtraction);
+  // printf("Multiply = %d\n", mult);
 
+  MPI_Finalize();
   return 0;
-
 }

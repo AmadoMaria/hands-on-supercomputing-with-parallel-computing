06b1eb182e630f6ff8799dc571fb45e606b88671
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-11-21 12:33:50 -0300

    add bash slurm

--- Hackathon/execution.sh
+++ Hackathon/execution.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
-dir="results_"
+!/bin/sh
+
+#SBATCH --job-name=hackathon                         # Job name
+#SBATCH --nodes=1                              # Run all processes on 2 nodes
+#SBATCH --partition=gpushortc                   # Partition OGBON
+#SBATCH --output=out_%j.log                    # Standard output and error log
+#SBATCH --ntasks-per-node=1                    # 1 job per node
+#SBATCH --account=treinamento                   # Account of the group
+
+
+
+dir="results"
 if [ ! -d "$dir" ]
 then
  mkdir $dir

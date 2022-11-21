ae5990002b7a397c79f7567e50d195943ebbd5be
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-11-20 22:55:03 -0300

    fix: cuda errors

--- Hands-On-7-Accelerating-Applications-with-CUDA-C-Python/fundamentals-CUDA-C/Accelerating-Applications-with-CUDA-C/09-heat/01-heat-conduction_gpu.cu
+++ Hands-On-7-Accelerating-Applications-with-CUDA-C-Python/fundamentals-CUDA-C/Accelerating-Applications-with-CUDA-C/09-heat/01-heat-conduction_gpu.cu
@@ -21,6 +21,40 @@ inline cudaError_t checkCuda(cudaError_t result)
   return result;
 }
 
+__global__ void step_kernel_mod(int ni, int nj, float fact, float *temp_in, float *temp_out)
+{
+  int i00, im10, ip10, i0m1, i0p1;
+  float d2tdx2, d2tdy2;
+
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+  int j = row;
+  int i = col;
+
+  // loop over all points in domain (except boundary)
+  if (j > 0 && j < nj - 1)
+  {
+    if (i > 0 && i < ni - 1)
+    {
+      // find indices into linear memory
+      // for central point and neighbours
+      i00 = I2D(ni, i, j);
+      im10 = I2D(ni, i - 1, j);
+      ip10 = I2D(ni, i + 1, j);
+      i0m1 = I2D(ni, i, j - 1);
+      i0p1 = I2D(ni, i, j + 1);
+
+      // evaluate derivatives
+      d2tdx2 = temp_in[im10] - 2 * temp_in[i00] + temp_in[ip10];
+      d2tdy2 = temp_in[i0m1] - 2 * temp_in[i00] + temp_in[i0p1];
+
+      // update temperatures
+      temp_out[i00] = temp_in[i00] + fact * (d2tdx2 + d2tdy2);
+    }
+  }
+}
+
 void step_kernel_ref(int ni, int nj, float fact, float *temp_in, float *temp_out)
 {
   int i00, im10, ip10, i0m1, i0p1;
@@ -49,39 +83,6 @@ void step_kernel_ref(int ni, int nj, float fact, float *temp_in, float *temp_out
   }
 }
 
-__global__ void step_kernel_mod(int ni, int nj, float fact, float *temp_in, float *temp_out)
-{
-
-  int j = threadIdx.x + blockIdx.x * blockDim.x;
-  int i = blockDim.x * gridDim.x;
-
-  int i00, im10, ip10, i0m1, i0p1;
-  float d2tdx2, d2tdy2;
-
-  // loop over all points in domain (except boundary)
-  // for ( int j=1; j < nj-1; j++ ) {
-  if (j < nj && i < nj)
-  {
-
-    // for ( int i=1; i < ni-1; i++ ) {
-    //  find indices into linear memory
-    //  for central point and neighbours
-    i00 = I2D(ni, i, j);
-    im10 = I2D(ni, i - 1, j);
-    ip10 = I2D(ni, i + 1, j);
-    i0m1 = I2D(ni, i, j - 1);
-    i0p1 = I2D(ni, i, j + 1);
-
-    // evaluate derivatives
-    d2tdx2 = temp_in[im10] - 2 * temp_in[i00] + temp_in[ip10];
-    d2tdy2 = temp_in[i0m1] - 2 * temp_in[i00] + temp_in[i0p1];
-
-    // update temperatures
-    temp_out[i00] = temp_in[i00] + fact * (d2tdx2 + d2tdy2);
-  }
-  //  }
-}
-
 int main()
 {
   int istep;
@@ -98,8 +99,11 @@ int main()
 
   temp1_ref = (float *)malloc(size);
   temp2_ref = (float *)malloc(size);
+
   checkCuda(cudaMallocManaged(&temp1, size));
   checkCuda(cudaMallocManaged(&temp2, size));
+
+  cudaError_t syncErr, asyncErr;
 
   // Initialize with random data
   for (int i = 0; i < ni * nj; ++i)
@@ -113,30 +117,38 @@ int main()
     step_kernel_ref(ni, nj, tfac, temp1_ref, temp2_ref);
 
     // swap the temperature pointers
-
     temp_tmp = temp1_ref;
     temp1_ref = temp2_ref;
     temp2_ref = temp_tmp;
   }
 
-  dim3 threads_per_block(ni, nj, 1);
+  dim3 threads_per_block(32, 32, 1); // 1024
   dim3 number_of_blocks(((ni * nj) / threads_per_block.x) + 1, ((ni * nj) / threads_per_block.y) + 1, 1);
+
   // Execute the modified version using same data
   for (istep = 0; istep < nstep; istep++)
   {
     step_kernel_mod<<<number_of_blocks, threads_per_block>>>(ni, nj, tfac, temp1, temp2);
 
+    syncErr = cudaGetLastError();
+    asyncErr = cudaDeviceSynchronize();
+
+    if (syncErr != cudaSuccess)
+      printf("Error: %s\n", cudaGetErrorString(syncErr));
+    if (asyncErr != cudaSuccess)
+      printf("Error: %s\n", cudaGetErrorString(asyncErr));
     // swap the temperature pointers
     temp_tmp = temp1;
     temp1 = temp2;
     temp2 = temp_tmp;
   }
-  cudaDeviceSynchronize();
 
   float maxError = 0;
   // Output should always be stored in the temp1 and temp1_ref at this point
   for (int i = 0; i < ni * nj; ++i)
   {
+    // printf("%.5f ", temp1[i]);
+    // printf("%.5f \n", temp1_ref[i]);
     if (abs(temp1[i] - temp1_ref[i]) > maxError)
     {
       maxError = abs(temp1[i] - temp1_ref[i]);
@@ -145,7 +157,9 @@ int main()
 
   // Check and see if our maxError is greater than an error bound
   if (maxError > 0.0005f)
+  {
     printf("Problem! The Max Error of %.5f is NOT within acceptable bounds.\n", maxError);
+  }
   else
     printf("The Max Error of %.5f is within acceptable bounds.\n", maxError);
 

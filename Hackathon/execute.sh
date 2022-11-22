1143fde7cba17446776b0396f1c709c67931d9e1
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-11-21 21:29:58 -0300

    separating

--- Hackathon/execute.sh
+++ Hackathon/execute.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH --job-name=553072                         # Job name
+#SBATCH --job-name=hackathon_execution                         # Job name
 #SBATCH --nodes=1                              # Run all processes on 2 nodes
 #SBATCH --partition=gpushortc                   # Partition OGBON
 #SBATCH --output=out_%j.log                    # Standard output and error log
 #SBATCH --ntasks-per-node=1                    # 1 job per node
-#SBATCH --account=treinamento                   # Account of the group
+#SBATCH --account=interact                   # Account of the group
 
 
 module load openmpi/4.1.1-cuda-11.6-ofed-5.4

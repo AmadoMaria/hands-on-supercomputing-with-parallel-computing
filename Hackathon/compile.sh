5eedf1184c36b9705f3c51b67b392988be751e19
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-11-25 22:40:24 -0300

    multiples passwords

--- Hackathon/compile.sh
+++ Hackathon/compile.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-#SBATCH --job-name=553072                         # Job name
+#SBATCH --job-name=compile_files                        # Job name
 #SBATCH --nodes=1                              # Run all processes on 2 nodes
 #SBATCH --partition=gpushortc                   # Partition OGBON
 #SBATCH --output=out_%j.log                    # Standard output and error log
 #SBATCH --ntasks-per-node=1                    # 1 job per node
-#SBATCH --account=treinamento                   # Account of the group
+#SBATCH --account=interact                   # Account of the group
 
 module load openmpi/4.1.1-cuda-11.6-ofed-5.4
 
-nvcc -I/opt/share/openmpi/4.1.1-cuda/include -L/opt/share/openmpi/4.1.1-cuda/lib64 -DprintLabel -lnccl -lmpi -Xcompiler -fopenmp -o bruteForce-mpi brute_force_mpi.c
+gcc brute_force_sequential.c -o bruteForce -std=c99 -O3
 
+gcc brute_force_openmp.c -o bruteForce-omp -fopenmp -std=c99 -O3
+
+mpicc brute_force_mpi.c -o bruteForce-mpi -fopenmp -std=c99 -O3
+
+mpicc brute_force_openmpi.c -o bruteForce-openmpi -fopenmp
+    
+nvcc brute_force_cuda.cu -o bruteForceGPU -x cu
---
+++ Hackathon/multiples_executions.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+dir="results_multiple"
+if [ ! -d "$dir" ]
+then
+ mkdir $dir
+fi
+
+creating_files(){
+    echo "password;seq;time;" >> ./${dir}/seq
+    echo "password;num_threads;time;" >> ./${dir}/omp
+    echo "password;num_process;time;" >> ./${dir}/mpi
+    echo "password;num_threads;num_process;time;" >> ./${dir}/openmpi
+    echo "password;num_blocks;time;" >> ./${dir}/cuda
+}
+
+seq_execution(){
+
+    seq=$(./bruteForce $1 | grep "seconds" | cut -d " " -f 1)
+
+    
+    echo "${1};1;${seq};" >> ./${dir}/seq${1}
+
+}
+
+omp_execution(){
+
+    omp=$(OMP_NUM_THREADS=$2 ./bruteForce-omp $1 | grep "seconds" | cut -d " " -f 1)
+    echo "${1};${2};${omp};" >> ./${dir}/omp
+
+}
+
+mpi_execution(){
+    mpirun -x MXM_LOG_LEVEL=error -np $2 --allow-run-as-root ./bruteForce-mpi $1 2>/dev/null > output_mpi
+    mpi=$(cat output_mpi | grep "seconds" | cut -d " " -f 1)
+    echo "${1};${2};${mpi};" >> ./${dir}/mpi    
+}
+
+openmpi_execution(){
+    OMP_NUM_THREADS=$2 mpirun -x MXM_LOG_LEVEL=error -np $3 ./bruteForce-openmpi $3 2>/dev/null > output_openmpi
+    ompi=$(cat output_openmpi | grep "seconds" | cut -d " " -f 1)
+    echo "${1};${2};${3};${ompi}" >> ./${dir}/openmpi
+}
+
+cuda_execution(){
+    cuda=$(./bruteForceGPU $1 | grep "seconds" | cut -d " " -f 1)
+    echo "${1};32;${cuda};" >> ./${dir}/cuda
+}
+
+main(){
+    creating_files
+    pass=$(
+    awk -F ";" '{
+        print $1
+    }' passwords)
+    for p in $pass;
+    do
+    echo "Executing brute force  algorithm for $p"
+    seq_execution $p
+    omp_execution $p $2
+    mpi_execution $p $3
+    openmpi_execution $p $2 $3
+    cuda_execution $p
+    
+    done
+}
